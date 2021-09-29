d0842ce168fdd16714d619aeb64b2958323bafa8
developedByQ <qtaskew@aggies.ncat.edu>
2021-09-29 14:03:41 -0400

    Added Routing Module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {AggieSignupComponent} from "./aggie-signup/aggie-signup.component";
+import {DriverSignupComponent} from "./driver-signup/driver-signup.component";
+import {VendorSignupComponent} from "./vendor-signup/vendor-signup.component";
+import {AggieSigninComponent} from "./aggie-signin/aggie-signin.component";
+import {DriverSigninComponent} from "./driver-signin/driver-signin.component";
+import {VendorSigninComponent} from "./vendor-signin/vendor-signin.component";
+import {ForgotPasswordComponent} from "./forgot-password/forgot-password.component";
+import {VendorHomeComponent} from "./vendor-home/vendor-home.component";
+import {AggieHomeComponent} from "./aggie-home/aggie-home.component";
+import {DriverHomeComponent} from "./driver-home/driver-home.component";
+
+const routes: Routes = [
+  { path: '',
+    component: AggieSignupComponent
+  },
+  { path: 'aggiesignup',
+    component: AggieSignupComponent
+  },
+  { path: 'driversignup',
+    component: DriverSignupComponent
+  },
+  { path: 'vendorsignup',
+    component: VendorSignupComponent
+  },
+  { path: 'aggiesignin',
+    component: AggieSigninComponent
+  },
+  { path: 'driversignin',
+    component: DriverSigninComponent
+  },
+  { path: 'vendorsignin',
+    component: VendorSigninComponent
+  },
+  { path: 'forgotpassword',
+    component: ForgotPasswordComponent
+  },
+  { path: 'vendorhome',
+    component: VendorHomeComponent
+  },
+  { path: 'aggiehome',
+    component: AggieHomeComponent
+  },
+  { path: 'driverhome',
+    component: DriverHomeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
