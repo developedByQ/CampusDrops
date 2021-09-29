6b0bbdd44dfddade4476025640fca2fbda26cee4
developedByQ <qtaskew@aggies.ncat.edu>
2021-09-29 14:07:09 -0400

    Added Models/ForgotPassword

---
+++ src/app/models/aggie.ts
@@ -0,0 +1,11 @@
+export class Aggie {
+
+  constructor(
+    public id: string,
+    public profileImageURL: string,
+    public firstName: string,
+    public lastName: string,
+    public email: string
+  ) {
+  }
+}
