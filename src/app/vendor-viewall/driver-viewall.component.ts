bd88427202b9ef29500fb972d54181619a5fb7cc
Nazere Wright <nazwrightthedeveloper@gmail.com>
2021-12-02 12:56:15 -0500

    temp fix - vendor uploaded items not loading

--- src/app/vendor-viewall/driver-viewall.component.ts
+++ src/app/vendor-viewall/driver-viewall.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
-import {MainService} from "../main.service";
-
+import { Router } from '@angular/router';
+import { MainService } from '../main.service';
 
 @Component({
   selector: 'app-driver-viewall',
   templateUrl: './driver-viewall.component.html',
-  styleUrls: ['./driver-viewall.component.css']
+  styleUrls: ['./driver-viewall.component.css'],
 })
 export class DriverViewallComponent implements OnInit {
-
   private route: Router;
   public currentVendor: string | undefined = '';
   public currentVenderproductList: any = [];
@@ -23,13 +21,12 @@ export class DriverViewallComponent implements OnInit {
     // console.log(this.currentVendor);
     this.service.dataRec.subscribe(() => {
       for (var item in this.service.ItemList) {
-        if (this.service.ItemList[item].vendor == "WbCXGlmEUgR4T4OAGsojTxbpz662") {
-          this.currentVenderproductList.push(this.service.ItemList[item]);
-        } else {
-          console.log(this.service.ItemList[item].vendor);
-        }
+        // if (this.service.ItemList[item].vendor == "WbCXGlmEUgR4T4OAGsojTxbpz662") {
+        this.currentVenderproductList.push(this.service.ItemList[item]);
+        // } else {
+        //   console.log(this.service.ItemList[item].vendor);
+        // }
       }
     });
   }
-
 }
